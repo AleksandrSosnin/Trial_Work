482d507d94f3cc0104d05e49957330bcb2b71dc1
Aleksandr <sosnin93@yandex.ru>
2023-10-03 15:15:50 +0300

    1

--- Trial/Program.cs
+++ Trial/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите элементы массива через запятую: ");
+        string input = (Console.ReadLine()!);
+        string[] inputArray = input.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+
+        string[] resultArray = FilterStrings(inputArray);
+
+        Console.WriteLine($"Конечный массив: [{string.Join(", ", FilterStrings(inputArray))}]");
+    }
+
+    static string[] FilterStrings(string[] inputArray)
+    {
+        int count = 0;
+        for (int i = 0; i < inputArray.Length; i++)
+        {
+            if (inputArray[i].Length <= 3)
+            {
+                count++;
+            }
+        }
+
+        string[] resultArray = new string[count];
+        int resultIndex = 0;
+        for (int i = 0; i < inputArray.Length; i++)
+        {
+            if (inputArray[i].Length <= 3)
+            {
+                resultArray[resultIndex] = inputArray[i];
+                resultIndex++;
+            }
+        }
+        return resultArray;
+    }
+}
